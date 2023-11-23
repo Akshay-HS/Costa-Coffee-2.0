c644aeed5d8279ea63eb6a003aff76356067b915
Akshay H S <akshay.hs@experionglobal.local>
2023-11-23 16:41:22 +0530

    feat:merchstor products

--- merch-store/js/merchStore.js
+++ merch-store/js/merchStore.js
@@ -1,21 +1,48 @@
-// Fetch data from the API and display details in the specified div
-fetch("https://fakestoreapi.com/products/1")
-  .then((res) => res.json())
-  .then((json) => {
-    // Access elements in the card
-    const cardImage = document.getElementById("cardImage");
-    const cardTitle = document.getElementById("cardTitle");
-    const cardCategory = document.getElementById("cardCategory");
-    const cardPrice = document.getElementById("cardPrice");
-    const cardDescription = document.getElementById("cardDescription");
+// Fetch the first 4 products from the API
+fetch('https://fakestoreapi.com/products')
+  .then(res => res.json())
+  .then(products => {
+    const leftProductsContainer = document.getElementById('leftProducts');
+    const rightProductsContainer = document.getElementById('rightProducts');
 
-    // Set values to fetched data
-    cardImage.src = json.image;
-    cardTitle.textContent = json.title;
-    cardCategory.textContent = `Category: ${json.category}`;
-    cardPrice.textContent = `Price: $${json.price}`;
-    cardDescription.textContent = json.description;
+    // Select the first 4 products
+    const selectedProducts = products.slice(0, 4);
+
+    // Iterate through the selected products
+    selectedProducts.forEach((product, index) => {
+      const productCard = document.createElement('div');
+      productCard.classList.add('card', 'mb-3', 'game-card-y-hover');
+      productCard.style.maxWidth = '540px';
+
+      // Extract the first 10 words of the description
+      const truncatedDescription = product.description.split(' ').slice(0, 10).join(' ');
+      const truncatedtitle = product.title.split(' ').slice(0, 10).join(' ');
+
+      productCard.innerHTML = `
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${product.image}" class="card-img-top" alt="Product Image">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${truncatedtitle}</h5>
+              <p class="card-text">Category: ${product.category}</p>
+              <p class="card-text">Price: $${product.price}</p>
+              <p class="card-text">${truncatedDescription}...</p>
+              <a href="#" class="btn btn-success">Buy Now</a>
+            </div>
+          </div>
+        </div>
+      `;
+
+      // Distribute cards between left and right containers
+      if (index < 2) {
+        leftProductsContainer.appendChild(productCard);
+      } else {
+        rightProductsContainer.appendChild(productCard);
+      }
+    });
   })
-  .catch((error) => {
-    console.log("Error fetching API data:", error);
+  .catch(error => {
+    console.log('Error fetching API data:', error);
   });
